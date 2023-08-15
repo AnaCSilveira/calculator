39354b6aafb23a2187e36696df774c8a24dc5a41
Ana Cristina Silveira <ana.cris372@gmail.com>
2023-08-14 22:28:29 -0300

    settings

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let operator = ""
 let numberTwo = ""
 
 
-function addition(n1,n2) {
+function add(n1,n2) {
     return n1 + n2
 }
 
-function subtraction(s1,s2) {
+function subtract(s1,s2) {
     return s1 - s2
 }
 
-function division(d1,d2) {
+function divide(d1,d2) {
     return d1 / d2
 }
 
-function multiplication(m1,m2) {
+function multiply(m1,m2) {
     return m1 * m2
 }
 
@@ -34,18 +34,21 @@ function registerOperation(x) {
 }
 
 function result(r) {
-    document.querySelector('.mini-screen').value += r
+    let minScreen = document.querySelector('.mini-screen')
+        minScreen.value += r
     const n1 = parseInt(numberOne)
     const n2 = parseInt(numberTwo)
+
     if (operator == '+') {
-        document.querySelector('.mini-screen').value = (addition(n1,n2))
+        minScreen.value = (add(n1,n2))
     } else if (operator == '-') {
-        document.querySelector('.mini-screen').value = (subtraction(n1,n2))
+        minScreen.value = (subtract(n1,n2))
     } else if (operator == '/') {
-        document.querySelector('.mini-screen').value = (division(n1,n2))
-    } else { document.querySelector('.mini-screen').value = (multiplication(n1,n2)) }
+        minScreen.value = (divide(n1,n2))
+    } else {minScreen.value = (multiply(n1,n2))}
 
 }
+
 
 
 
